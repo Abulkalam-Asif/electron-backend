a99a3933e7303ed1c27291c44f408aadcabeb7a9
Muhammad Jafar <103287122+Sniper512@users.noreply.github.com>
2025-04-14 23:35:26 +0500

    Dynamic imports added

--- src/utils/signJwt.ts
+++ src/utils/signJwt.ts
@@ -1,14 +1,15 @@
-import * as jose from "jose";
-
 export const signJwt = async (username: string) => {
-  const token = new jose.SignJWT({
-    username,
-  })
-    .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setExpirationTime("1h");
+	// Import jose dynamically
+	const jose = await import("jose");
 
-  const secret = process.env.JWT_SECRET;
-  if (!secret) throw new Error("JWT secret is not defined");
-  return token.sign(new TextEncoder().encode(secret));
+	const token = new jose.SignJWT({
+		username,
+	})
+		.setProtectedHeader({ alg: "HS256" })
+		.setIssuedAt()
+		.setExpirationTime("1h");
+
+	const secret = process.env.JWT_SECRET;
+	if (!secret) throw new Error("JWT secret is not defined");
+	return token.sign(new TextEncoder().encode(secret));
 };
--- src/utils/verifyJwt.ts
+++ src/utils/verifyJwt.ts
@@ -1,15 +1,18 @@
-import * as jose from 'jose'
+// Using dynamic import for ESM compatibility
 
 export const verifyJwt = async (token: string) => {
-  const secret = process.env.JWT_SECRET
-  if (!secret) throw new Error('JWT secret is not defined')
-  try {
-    await jose.jwtVerify(token, new TextEncoder().encode(secret), {
-      algorithms: ['HS256']
-    })
-    return true
-  } catch (error) {
-    console.error('JWT verification failed', error)
-    throw new Error('JWT verification failed')
-  }
-}
+	// Import jose dynamically
+	const jose = await import("jose");
+
+	const secret = process.env.JWT_SECRET;
+	if (!secret) throw new Error("JWT secret is not defined");
+	try {
+		await jose.jwtVerify(token, new TextEncoder().encode(secret), {
+			algorithms: ["HS256"],
+		});
+		return true;
+	} catch (error) {
+		console.error("JWT verification failed", error);
+		throw new Error("JWT verification failed");
+	}
+};
